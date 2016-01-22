511c7023fdea5ef06546a2ec48db890cf10a5fc2
Dmitry Ponyatov <dponyatov@gmail.com>
2016-01-23 00:01:56 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- bat.bat
+++ bat.bat
@@ -1,2 +1,2 @@
 @start .
-@gvim -p do.do log.log ypp.ypp lpp.lpp hpp.hpp cpp.cpp win32.hpp win32.cpp Makefile bat.bat README.md .gitignore 
+@gvim -p do.do lexfiles.do log.log ypp.ypp lpp.lpp hpp.hpp cpp.cpp win32.hpp win32.cpp Makefile bat.bat README.md .gitignore 
