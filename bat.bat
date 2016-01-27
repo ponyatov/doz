a876f4739519cf6c793dadcdae0850b9e97fca4d
Dmitry Ponyatov <dponyatov@gmail.com>
2016-01-27 17:28:31 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- bat.bat
+++ bat.bat
@@ -1,2 +1,2 @@
 @start .
-@gvim -p dos.bI ../Y/lexfiles.do dos.blog ../Y/ypp.ypp ../Y/lpp.lpp meta.hpp ../Y/hpp.hpp ../Y/cpp.cpp ../Y/win32.hpp ../Y/win32.cpp Makefile bat.bat README.md .gitignore 
+@gvim -p dos.bI ../Y/lex/files.bI dos.blog ../Y/ypp.ypp ../Y/lpp.lpp meta.hpp ../Y/hpp.hpp ../Y/cpp.cpp ../Y/win32.hpp ../Y/win32.cpp Makefile bat.bat README.md .gitignore 
